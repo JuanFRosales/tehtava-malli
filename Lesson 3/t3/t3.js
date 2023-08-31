7a964f1fde01612bb2c6f9d9848294fcabda64a6
Juan <juanRosalesMakinen@metropolia.fi>
2023-08-31 10:58:47 +0300

    code simplifying

--- Lesson 3/t3/t3.js
+++ Lesson 3/t3/t3.js
@@ -4,21 +4,9 @@
 const targetElement = document.getElementById('target');
 
 
-// Get browser version from user agent
+// Get browser version 
 const userAgent = window.navigator.userAgent;
-const browserVersion = userAgent.match(/(Chrome|Firefox|Safari|Edge|IE|Opera|Trident(?=\/))\/?\s*([\d\.]+)/i);
-
-
-
-if (browserVersion && browserVersion.length >= 3) {
-  const browserName = browserVersion[1];
-  const version = browserVersion[2];
-  createParagraph(`Browser name: ${browserName}, version: ${version}`);
-} else {
-  createParagraph("Browser version information not found.");
-}
-
-
+c
 // Get screen information
 const fullScreenWidth = screen.width;
 const fullScreenHeight = screen.height;
@@ -36,20 +24,18 @@ const heightDifference = fullScreenHeight - windowInfo.outerHeight;
 
 //Date and time
 const currentDate = new Date();
-
-//Get actual hours and minutes
-const currentHours = currentDate.getHours();
-const currentMinutes = currentDate.getMinutes();
-
-//Date format including hours and minutes
-const date = new Date(2056, 1, 1, currentHours, currentMinutes, 16, 738);
-
+const options ={
+  year: 'numeric',
+  month: 'long',
+  day : 'numeric',
+};
 
 //Place each item within its own <p>, browser info done previously
+createParagraph(`${userAgent}`)
 createParagraph(`Screen Information: Width:${(window.screen.width)}, Height:${(window.screen.height) }`);
 createParagraph(`Available width for Browser: ${widthDifference}`);
 createParagraph(`Available height for Browser: ${heightDifference}`);
-createParagraph(`${date}`)
+createParagraph(`Date: ${currentDate.toLocaleDateString('fi-FI', options)}`)
 
 
 //Placing it to HTML
