fe5240e37a10571504e26df5a4db9adc6dcec7da
Juan <juanRosalesMakinen@metropolia.fi>
2023-10-15 18:42:48 +0300

    version 1.0

---
+++ RestaurantApp/src/interfaces/Menu.ts
@@ -0,0 +1,16 @@
+interface weeklyMenu {
+  days: Menu[]
+}
+
+interface Menu {
+  date: string;
+  courses: Course[],
+}
+
+interface Course {
+  price: string;
+  name: string;
+  diets: string[];
+}
+
+export type {Menu, weeklyMenu}
