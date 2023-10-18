87556a2cd39765c77c87f0dc6a4b489e5f673e7b
Juan <juanRosalesMakinen@metropolia.fi>
2023-10-18 20:49:33 +0300

    version 3.0

--- Restaurant-app/dev-dist/sw.js
+++ Restaurant-app/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.iab3dt9irko"
+    "revision": "0.njftap204q8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
